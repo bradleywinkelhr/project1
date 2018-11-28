b483805b387b9847f8cac954f4a60af13218964d
AdeshNakorikantodas <45424258+AdeshNakorikantodas@users.noreply.github.com>
2018-11-28 16:10:52 +0100

    i got hoeess

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 s = 0
 def nibba():
-    if s == 0:
-        print ("I got hoessssssssssss")
+    if s % 2 == 0:
+          print ("I got hoessssssssssss")
+    
+def white_nibba():
+    if s % 3 == 0:
+        print("Callingggggggggggg")
+
+while s < 11:
+    if s % 2 == 0:
+        s = s + 1
+        nibba()
     else:
-        print("hit or miss")
-nibba()
+         white_nibba()
+
+        
+
+
+
+        
+    
 
 
